bd9df4af5557719d96630f44fbf070839a48b39e
Guillermo Moya <gmoya@unal.edu.co>
2024-05-01 11:06:17 -0500

    Crear carpetas

---
+++ app/Convocatorias/ConvocatoriasAdmin/VerPostulaciones/page.jsx
@@ -0,0 +1,23 @@
+import CardPostulation from '@/components/ChooseWinners/CardPostulation'
+import Link from 'next/link'
+import React from 'react'
+
+function page() {
+  return (
+    
+
+    <><div className='CSS code'>  page</div>
+    <CardPostulation  id= {2}></CardPostulation>
+
+    <Link href=""><button>
+        Da Clic.
+        </button></Link>
+    </>
+
+
+    //Escribir HTML
+
+  ) 
+}
+
+export default page
---
+++ components/ChooseWinner/CardPostulation.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function CardPostulation({id}) {
+
+
+  return (
+    <div>CardPostulation
+        {id}
+    </div>
+
+    
+  )
+}
+
+export default CardPostulation
