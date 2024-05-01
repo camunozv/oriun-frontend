cce11975217deb269f65c67c9e394f71698186a6
Guillermo Moya <gmoya@unal.edu.co>
2024-05-01 13:07:30 -0500

    Base de la page y las cardPostulation

--- components/ChooseWinner/CardPostulation.jsx
+++ components/ChooseWinner/CardPostulation.jsx
@@ -1,15 +1,74 @@
+"use client";
+
 import React from 'react'
+import Link from "next/link";
 
-function CardPostulation({id}) {
+function CardPostulation({
+  // Aqui se agregan las variables necesarias para la carta de postulacion.
+  // students open & closed
+  id,
+  university_name,
+  country,
+  language, //* shared characteristics
+  deadline, //* shared characteristics
 
+  // admin open & closed
+  semester,
+  study_level,
+  year,
 
-  return (
-    <div>CardPostulation
-        {id}
-    </div>
+  open,
+  admin,
+})  {
+  if (admin === true) {
+    return (
+      <>
+        <div className="flex justify-center items-center flex-col gap-3 bg-white shadow-lg rounded-xl p-6 w-full h-full">
+          <h6 className="font-bold">{id}</h6>
+          <div className="w-[95%] h-45 bg-figma_grey text-black rounded-lg">
+            Flag Here Flag Here Flag Here Flag Here Flag Here Flag Here Flag
+            Here Flag Here Flag Here Flag Here Flag Here Flag Here Flag Here
+            Flag Here Flag Here Flag Here Flag Here Flag Here Flag Here Flag
+            Here Flag Here Flag Here Flag Here Flag Here Flag Here Flag Here
+            Flag Here Flag Here Flag Here Flag Here Flag Here Flag Here Flag
+            Here Flag Here Flag Here Flag Here Flag Here Flag Here Flag Here
+            Flag Here Flag Here Flag Here Flag Here Flag Here
+          </div>
 
-    
-  )
+          <p className="font-semibold">
+            {year} - {semester}
+          </p>
+          <p className="font-semibold">{language}</p>
+          <p className="font-semibold">{study_level}</p>
+
+          <div className="flex justify-between items-center w-96">
+            <div className="w-full p-2">
+              <Link href={`/Convocatorias/ConvocatoriasAdmin/${id}`}>
+                <button
+                  type="button"
+                  className="w-full font-semibold bg-white border-2 rounded-full border-figma_blue text-figma_blue py-2"
+                >
+                  Actualizar
+                </button>
+              </Link>
+            </div>
+            <div className="w-full p-2">
+              <Link
+                href={`/Convocatorias/ConvocatoriasAdmin/DetallesConvocatoria/${id}`}
+              >
+                <button
+                  type="button"
+                  className="w-full font-semibold bg-figma_blue border-2 rounded-full border-figma_blue text-white py-2"
+                >
+                  Ver detalles
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default CardPostulation
