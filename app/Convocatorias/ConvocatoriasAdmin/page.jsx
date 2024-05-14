83cfe7c1e9749f8828659c254b50db46fefea97a
RocketBoe <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2024-05-13 20:57:06 -0500

    fix route protection student

--- app/Convocatorias/ConvocatoriasAdmin/page.jsx
+++ app/Convocatorias/ConvocatoriasAdmin/page.jsx
@@ -4,11 +4,18 @@ import { useSession } from "next-auth/react";
 import { redirect } from "next/dist/server/api-utils";
 
 function ConvocatoriasAdminPage() {
-  const session = useSession();
+  const { data: session, status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      redirect("/Convocatorias");
+    },
+  });
   const user_type = session?.type_user;
 
-  if (user_type === "student") {
-    redirect('/Convocatorias');
+  if (!session) {
+    return <div>{status}...</div>;
+  } else if (user_type === "student") {
+    redirect("/Convocatorias");
   } else {
     return (
       <main className="relative mt-4 mx-auto overflow-hidden max-w-[1580px] gap-3 p-2">
