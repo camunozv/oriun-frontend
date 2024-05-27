9cef799faff99d348af078fdb372179100c422da
maromana <maromana@unal.edu.co>
2024-05-27 16:32:37 -0500

    explication of images for user

--- app/Convocatorias/ConvocatoriasAdmin/VerResultadosConv/page.jsx
+++ app/Convocatorias/ConvocatoriasAdmin/VerResultadosConv/page.jsx
@@ -7,6 +7,19 @@ function ResultConvAdmin(){
         <div className="p-8">
             <h1 className="font-bold text-[40px] underline text-center">Conv No</h1>
             <br/>
+            <div className="flex grid grid-cols-2 justify-center">
+                <div className="px-3">
+                    <p className="font-bold text-[20px] text-right">Aprobado 
+                        <img src={"/images/Aprobada.jpeg"} className="inline object-contain h-10 w-10"/> 
+                    </p>
+                </div>
+                <div className="px-3">
+                    <p className="font-bold text-[20px] text-left"> No Aprobado 
+                        <img src={"/images/NoAprobada.jpeg"} className="inline object-contain h-10 w-10"/> 
+                    </p>
+                </div>
+            </div>
+            <br/>
             <div className="grid grid-cols-3">
                 <div className="p-2">
                    <CardResultadosEmp StudentResult="Aprobado"/> 
--- app/Convocatorias/ConvocatoriasEstudiante/MisConvocatorias/page.jsx
+++ app/Convocatorias/ConvocatoriasEstudiante/MisConvocatorias/page.jsx
@@ -43,6 +43,29 @@ function MyCallsPage() {
     return (
       <>
         <main className="relative mt-4 mx-auto overflow-hidden max-w-[1580px] gap-3 p-2">
+          <div className="flex grid grid-cols-4 justify-center p-6">
+                <div>
+                    <p className="font-bold text-[20px] text-right">Aprobado 
+                        <img src={"/images/Aprobada.jpeg"} className="inline object-contain h-10 w-10"/> 
+                    </p>
+                </div>
+                <div>
+                    <p className="font-bold text-[20px] text-center pr-5"> No Aprobado 
+                        <img src={"/images/NoAprobada.jpeg"} className="inline object-contain h-10 w-10"/> 
+                    </p>
+                </div>
+                <div>
+                    <p className="font-bold text-[20px] text-left pr-5"> Pendiente por Revisión
+                        <img src={"/images/Pendiente.jpeg"} className="inline object-contain h-10 w-10"/> 
+                    </p>
+                </div>
+                <div>
+                    <p className="font-bold text-[20px] text-start"> Modificación Solicitada
+                        <img src={"/images/ModificacionSolicitada.jpeg"} className="inline object-contain h-10 w-10"/> 
+                    </p>
+                </div>
+          </div> 
+          <br/>
           <div className="grid grid-cols-3 w-full gap-6">
             {myApplications?.map((application) => (
               <ApplicationCard
