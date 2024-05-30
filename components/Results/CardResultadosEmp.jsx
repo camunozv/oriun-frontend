43329b1c3d863103f4465f79573c2e3ef9b5f8c1
RocketBoe <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2024-05-29 20:12:21 -0500

    Endpoint 2 use case 8 done, CORS problems, not tested

--- components/Results/CardResultadosEmp.jsx
+++ components/Results/CardResultadosEmp.jsx
@@ -1,14 +1,9 @@
-"use client";
-
 import React from "react";
-import Link from "next/link";
-import Image from "next/image";
-import { useEffect } from "react";
 
-function CardPostulacionEm({StudentResult}) {
+function CardPostulacionEm({student_id, student_name, university_name, university_country, call, approved}) {
   let imagenSrc;
 
-  switch (StudentResult) {
+  switch (approved) {
     case "Aprobado":
       imagenSrc = "/images/Aprobada.jpeg";
       break;
@@ -21,9 +16,7 @@ function CardPostulacionEm({StudentResult}) {
       <>
         <div className="flex justify-center items-center flex-col gap-3 bg-white shadow-lg rounded-xl p-6 w-full h-full">
           <div className="relative flex justify-center items-center flex-col gap-3 bg-white -lg rounded-xl p-6 w-full h-full">
-            <h6 className="font-bold">Conv No. </h6>
-
-
+            <h6 className="font-bold">Conv No. {call} </h6>
             <img
               src={imagenSrc} // Reemplaza esta ruta con la ruta de tu imagen
               alt="Logo"
@@ -41,12 +34,12 @@ function CardPostulacionEm({StudentResult}) {
             Flag Here Flag Here Flag Here Flag Here Flag Here
           </div>
 
-          <p className="font-semibold">ID Estudiante</p>
+          <p className="font-semibold">{student_id}</p>
           <p className="font-semibold">
-            Nombre del estudiante
+            {student_name}
           </p>
-          <p className="font-semibold">Nombre Universidad</p>
-          <p className="font-semibold">País</p>
+          <p className="font-semibold">{university_name}</p>
+          <p className="font-semibold">{university_country}</p>
         </div>
       </>
     );
