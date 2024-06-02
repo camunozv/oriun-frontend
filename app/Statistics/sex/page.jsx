7a8c9c85682b8dae602cc7b068c540f58bbfce1a
RocketBoe <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2024-06-02 16:53:12 -0500

    Sex page finished

--- app/Statistics/sex/page.jsx
+++ app/Statistics/sex/page.jsx
@@ -1,9 +1,13 @@
-"use client"
-import React from "react";
-import { Chart } from 'react-google-charts';
+"use client";
+import { apiCharts } from "@/app/api/Graficas/charts";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+import React, { useState } from "react";
+import { Chart } from "react-google-charts";
+import { useForm } from "react-hook-form";
 
-function renderChart(call_field, dataList, dataListCallField, h_axis, title){
-    /*
+function renderChart(call_field, dataList, dataListCallField, h_axis, title) {
+  /*
     Args:
     - call_field (string): Nombre Del campo del filtro de la convocatoria que se mostrará en la tabla
     - dataList (List of dict): Datos para llenar la tabla. Cada diccionario debe tener info. para los campos a visualizar
@@ -11,204 +15,212 @@ function renderChart(call_field, dataList, dataListCallField, h_axis, title){
     - h_axis (string): Titulo del eje x
     - title (string): Titulo de la gráfica
      */
-    const data =  [
-        [call_field, 'Masculino', 'Femenino']
+  const data = [[call_field, "Masculino", "Femenino"]];
+
+  for (let i = 0; i < dataList.length; i++) {
+    const row = [
+      dataList[i][dataListCallField],
+      dataList[i]["Masculino"],
+      dataList[i]["Femenino"],
     ];
+    data.push(row);
+  }
 
-    for (let i = 0; i < dataList.length; i++) {
-        const row = [
-            dataList[i][dataListCallField],
-            dataList[i]['Masculino'],
-            dataList[i]['Femenino']
-        ];
-        data.push(row);
-    }
+  const options = {
+    title: title,
+    allowHtml: true,
+    alternatingRowStyle: true,
+    width: "100%",
+    height: "100%",
+    sort: "enable",
+    backgroundColor: "#f7f7f7",
+    hAxis: {
+      title: h_axis,
+      scrollbar: "true",
+    },
+    vAxis: {
+      title: "Número Estudiantes",
+    },
+    animation: {
+      startup: true,
+      duration: 1500,
+      easing: "out",
+    },
+  };
 
-    const options = {
-        title: title,
-        allowHtml: true,
-        alternatingRowStyle: true,
-        width: '100%',
-        height: '100%',
-        sort: 'enable',
-        backgroundColor: '#f7f7f7',
-        hAxis: {
-            title: h_axis,
-            scrollbar: 'true'
-        },
-        vAxis: {
-            title: 'Número Estudiantes',
-        },
-        animation: {
-            startup: true,
-            duration: 1500,
-            easing: 'out',
-        },
-    };
-
-    return [data, options]
+  return [data, options];
 }
 
-//---------------------------------------------------------
-//---------------------------------------------------------
-//---------------------------------------------------------
-
-const dataList_univ_sex_post = [
-    {'university': 'UNAL', 'Masculino': 100, 'Femenino': 80},
-    {'university': 'UNIANDES', 'Masculino': 70, 'Femenino': 80},
-];
-const results_univ_sex_post = renderChart("Universidad", dataList_univ_sex_post, 'university', 'Universidades', 'Estadística: Universidades por género estudiantes postulados');
-
-const dataList_univ_sex_win = [
-    {'university': 'UNAL', 'Masculino': 50, 'Femenino': 40},
-    {'university': 'UNIANDES', 'Masculino': 30, 'Femenino': 40},
-];
-const results_univ_sex_win = renderChart("Universidad", dataList_univ_sex_win, 'university', 'Universidades', 'Estadística: Universidades por género estudiantes ganadores');
-
-//----------------------//----------------------//----------
-//----------------------//----------------------//----------
-
-const dataList_cou_sex_post = [
-    {'country': 'Mexico', 'Masculino': 100, 'Femenino': 80},
-    {'country': 'Canadá', 'Masculino': 70, 'Femenino': 80},
-    {'country': 'Francia', 'Masculino': 70, 'Femenino': 80},
-];
-const results_cou_sex_post = renderChart("País", dataList_cou_sex_post, 'country', 'Países', 'Estadística: Países por género estudiantes postulados');
-
-const dataList_cou_sex_win = [
-    {'country': 'Mexico', 'Masculino': 10, 'Femenino': 12},
-    {'country': 'Canadá', 'Masculino': 15, 'Femenino': 12},
-    {'country': 'Francia', 'Masculino': 12, 'Femenino': 15},
-];
-const results_cou_sex_win = renderChart("País", dataList_cou_sex_win, 'country', 'Países', 'Estadística: Países por género estudiantes ganadores');
-
-//----------------------//----------------------//----------
-//----------------------//----------------------//----------
-
-const dataList_reg_sex_post = [
-    {'region': 'Uniandes', 'Masculino': 100, 'Femenino': 80},
-    {'region': 'Asia', 'Masculino': 100, 'Femenino': 80},
-    {'region': 'Norteamérica', 'Masculino': 70, 'Femenino': 80},
-    {'region': 'Europa', 'Masculino': 70, 'Femenino': 80},
-    {'region': 'Nacional-Sigueme', 'Masculino': 100, 'Femenino': 80},
-    {'region': 'Oceanía', 'Masculino': 100, 'Femenino': 80},
-];
-const results_reg_sex_post = renderChart("Región", dataList_reg_sex_post, 'region', 'Regiones', 'Estadística: Regiones por género estudiantes postulados');
-
-const dataList_reg_sex_win = [
-    {'region': 'Uniandes', 'Masculino': 10, 'Femenino': 12},
-    {'region': 'Asia', 'Masculino': 15, 'Femenino': 12},
-    {'region': 'Norteamérica', 'Masculino': 15, 'Femenino': 12},
-    {'region': 'Europa', 'Masculino': 15, 'Femenino': 12},
-    {'region': 'Nacional-Sigueme', 'Masculino': 10, 'Femenino': 12},
-    {'region': 'Oceanía', 'Masculino': 10, 'Femenino': 12},
-];
-const results_reg_sex_win = renderChart("Región", dataList_reg_sex_win, 'region', 'Regiones', 'Estadística: regiones por género estudiantes ganadores');
-
-//----------------------//----------------------//----------
-//----------------------//----------------------//----------
-
-const dataList_yea_sex_post = [
-    {'year': '2023', 'Masculino': 100, 'Femenino': 80},
-    {'year': '2024', 'Masculino': 70, 'Femenino': 80},
-];
-const results_yea_sex_post = renderChart("Año", dataList_yea_sex_post, 'year', 'Años', 'Estadística: Años por género estudiantes postulados');
-
-const dataList_yea_sex_win = [
-    {'year': '2023', 'Masculino': 10, 'Femenino': 12},
-    {'year': '2024', 'Masculino': 15, 'Femenino': 12},
-];
-const results_yea_sex_win = renderChart("Año", dataList_yea_sex_win, 'year', 'Años', 'Estadística: Años por género estudiantes ganadores');
-
-//----------------------//----------------------//----------
-//----------------------//----------------------//----------
-
-const dataList_sem_sex_post = [
-    {'semester': '1', 'Masculino': 100, 'Femenino': 80},
-    {'semester': '2', 'Masculino': 70, 'Femenino': 80},
-];
-const results_sem_sex_post = renderChart("Semestre", dataList_sem_sex_post, 'semester', 'Semestres', 'Estadística: Semestres por género estudiantes postulados');
-
-const dataList_sem_sex_win = [
-    {'semester': '1', 'Masculino': 10, 'Femenino': 12},
-    {'semester': '2', 'Masculino': 15, 'Femenino': 12},
-];
-const results_sem_sex_win = renderChart("Semestre", dataList_sem_sex_win, 'semester', 'Semestres', 'Estadística: Semestres por género estudiantes ganadores');
-
-//---------------------------------------------------------
-//---------------------------------------------------------
-
 function App() {
-    const mystyle0 = {
-        color: "white",
-        backgroundColor: "DodgerBlue",
-        padding: "10px",
-        fontFamily: "Arial",
-        margin: "0 auto",
-    };
-    const mystyle = {
-        color: "white",
-        backgroundColor: "DodgerBlue",
-        padding: "10px",
-        fontFamily: "Arial",
-        margin: "0 auto",
-        width: "80%",
-    };
-    const columnChartStyle = {
-        width: "80%",
-        height: "400px",
-        margin: "0 auto"
+  const mystyle0 = {
+    color: "white",
+    backgroundColor: "DodgerBlue",
+    padding: "10px",
+    fontFamily: "Arial",
+    margin: "0 auto",
+  };
+  const mystyle = {
+    color: "white",
+    backgroundColor: "DodgerBlue",
+    padding: "10px",
+    fontFamily: "Arial",
+    margin: "0 auto",
+    width: "80%",
+  };
+  const columnChartStyle = {
+    width: "80%",
+    height: "400px",
+    margin: "0 auto",
+  };
+
+  const { data: session, state } = useSession({
+    required: true,
+    onUnauthenticated() {
+      redirect("/Convocatorias");
+    },
+  });
+
+  const token = session?.access;
+
+  const { register, reset, handleSubmit } = useForm();
+
+  const [dataToGraph, setDataToGraph] = useState([]);
+  const [renderSettings, setRenderSettings] = useState({
+    caso: "",
+    field: "",
+    title: "",
+  });
+  const my_submit = handleSubmit((data) => {
+    if (data.chart_parameter === "seleccion") {
+      alert("Ningun parámetro de gráfica seleccionado.");
+      return;
     }
-    return (
-        <div className="py-10">
-            <h2 style={mystyle0}><b>Estadísticas por género</b></h2>
-            <br/>
-            <h2 style={mystyle}><b>Universidades</b></h2>
-            <br/>
-            <div style={columnChartStyle}><Chart chartType="ColumnChart" data={results_univ_sex_post[0]}
-                                                 options={results_univ_sex_post[1]}/></div>
-            <br/>
-            <div style={columnChartStyle}><Chart chartType="ColumnChart" data={results_univ_sex_win[0]}
-                                                 options={results_univ_sex_win[1]}/></div>
-            <br/>
+    if (data.winner_state === "seleccion") {
+      alert("Ningun parámetro de estado de ganador seleccionado.");
+      return;
+    }
 
-            <h2 style={mystyle}><b>Países</b></h2>
-            <br/>
-            <div style={columnChartStyle}><Chart chartType="ColumnChart" data={results_cou_sex_post[0]}
-                                                 options={results_cou_sex_post[1]}/></div>
-            <br/>
-            <div style={columnChartStyle}><Chart chartType="ColumnChart" data={results_cou_sex_win[0]}
-                                                 options={results_cou_sex_win[1]}/></div>
-            <br/>
+    switch (data.chart_parameter) {
+      case "university":
+        setRenderSettings({
+          caso: "Universidades",
+          field: "university",
+          title: `Estudiantes ${data.winner_state} por universidad.`,
+        });
+        break;
+      case "semester":
+        setRenderSettings({
+          caso: "Semestre",
+          field: "semester",
+          title: `Estudiantes ${data.winner_state} por semestre.`,
+        });
+        break;
+      case "region":
+        setRenderSettings({
+          caso: "Region",
+          field: "region",
+          title: `Estudiantes ${data.winner_state} por region.`,
+        });
+        break;
+      default:
+        setRenderSettings({
+          caso: "País",
+          field: "country",
+          title: `Estudiantes ${data.winner_state} por país.`,
+        });
+    }
 
-            <h2 style={mystyle}><b>Regiones</b></h2>
-            <br/>
-            <div style={columnChartStyle}><Chart chartType="ColumnChart" data={results_reg_sex_post[0]}
-                                                 options={results_reg_sex_post[1]}/></div>
-            <br/>
-            <div style={columnChartStyle}><Chart chartType="ColumnChart" data={results_reg_sex_win[0]}
-                                                 options={results_reg_sex_win[1]}/></div>
-            <br/>
+    if (data.winner_state === "postulates") {
+      apiCharts
+        .getInfoCharts("sex", data.chart_parameter, token)
+        .then((response) => {
+          setDataToGraph(response.data.postulates);
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      apiCharts
+        .getInfoCharts("sex", data.chart_parameter, token)
+        .then((response) => {
+          setDataToGraph(response.data.winners);
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
 
-            <h2 style={mystyle}><b>Años</b></h2>
-            <br/>
-            <div style={columnChartStyle}><Chart chartType="ColumnChart" data={results_yea_sex_post[0]}
-                                                 options={results_yea_sex_post[1]}/></div>
-            <br/>
-            <div style={columnChartStyle}><Chart chartType="ColumnChart" data={results_yea_sex_win[0]}
-                                                 options={results_yea_sex_win[1]}/></div>
-            <br/>
+    reset();
+  });
 
-            <h2 style={mystyle}><b>Semestres</b></h2>
-            <br/>
-            <div style={columnChartStyle}><Chart chartType="ColumnChart" data={results_sem_sex_post[0]}
-                                                 options={results_sem_sex_post[1]}/></div>
-            <br/>
-            <div style={columnChartStyle}><Chart chartType="ColumnChart" data={results_sem_sex_win[0]}
-                                                 options={results_sem_sex_win[1]}/></div>
-            <br/>
-        </div>
-    )
+  let results = [];
+  if (dataToGraph?.length !== 0) {
+    results = renderChart(
+      renderSettings.caso,
+      dataToGraph,
+      renderSettings.field,
+      renderSettings.caso,
+      renderSettings.title
+    );
+  }
+
+  return (
+    <div className="py-10">
+      <div className="flex justify-between items-center" style={mystyle0}>
+        <h2>
+          <b>Estadísticas por Sexo</b>
+        </h2>
+        <form onSubmit={my_submit} className="flex gap-3 p-6">
+          <select
+            {...register("chart_parameter")}
+            className="bg-white rounded-lg text-blue-500 h-10"
+          >
+            <option value="seleccion">Selección</option>
+            <option value="university">Universidad</option>
+            <option value="semester">Semestre</option>
+            <option value="region">Región</option>
+            <option value="country">País</option>
+          </select>
+
+          <select
+            {...register("winner_state")}
+            className="bg-white rounded-lg text-blue-500 h-10"
+          >
+            <option value="seleccion">Selección</option>
+            <option value="postulates">Postulados</option>
+            <option value="winners">Ganadores</option>
+          </select>
+
+          <button
+            type="submit"
+            className="text-center justify-center flex items-center bg-white rounded-lg text-blue-500 h-10 p-5"
+          >
+            Buscar
+          </button>
+        </form>
+      </div>
+
+      <br />
+      <h2 style={mystyle}>
+        <b>{renderSettings.caso}</b>
+      </h2>
+      <br />
+      <div style={columnChartStyle}>
+        {results.length !== 0 ? (
+          <Chart
+            chartType="ColumnChart"
+            data={results[0]}
+            options={results[1]}
+          />
+        ) : (
+          <>No data to show</>
+        )}
+      </div>
+      <br />
+    </div>
+  );
 }
 
 export default App;
